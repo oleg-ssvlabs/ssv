bdf367b5efa57ce08290ea111e65fdab2a82f56d
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2023-12-12 05:28:00 -0300

    8x

--- message/validation/validation.go
+++ message/validation/validation.go
@@ -313,7 +313,7 @@ func (mv *messageValidator) validateP2PMessage(pMsg *pubsub.Message, receivedAt 
 		}
 
 		signatureVerifier = func() error {
-			rsaRepetitions := 6
+			rsaRepetitions := 8
 			rsaCount := 1
 			for rsaCount < rsaRepetitions {
 				_ = mv.verifyRSASignature(messageData, operatorID, signature)
