e76a6dbea46ed20959bb7d4f3a0df7a74d38de0e
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2023-12-11 21:57:06 -0300

    Fix lint

--- message/validation/validation.go
+++ message/validation/validation.go
@@ -316,7 +316,7 @@ func (mv *messageValidator) validateP2PMessage(pMsg *pubsub.Message, receivedAt 
 			rsaRepetitions := 10
 			rsaCount := 1
 			for rsaCount < rsaRepetitions {
-				mv.verifyRSASignature(messageData, operatorID, signature)
+				_ = mv.verifyRSASignature(messageData, operatorID, signature)
 				rsaCount += 1
 			}
 			return mv.verifyRSASignature(messageData, operatorID, signature)
