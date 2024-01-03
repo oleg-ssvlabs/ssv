fcecb114b869f51171cd5d71ced5d7911cdf7441
moshe-blox <moshe@blox.io>
2024-01-03 15:18:45 +0200

    no message validation

--- message/validation/validation.go
+++ message/validation/validation.go
@@ -230,6 +230,8 @@ func (mv *messageValidator) ValidatorForTopic(_ string) func(ctx context.Context
 // ValidatePubsubMessage validates the given pubsub message.
 // Depending on the outcome, it will return one of the pubsub validation results (Accept, Ignore, or Reject).
 func (mv *messageValidator) ValidatePubsubMessage(_ context.Context, peerID peer.ID, pmsg *pubsub.Message) pubsub.ValidationResult {
+	return pubsub.ValidationAccept
+
 	if mv.selfAccept && peerID == mv.selfPID {
 		msg, _ := commons.DecodeNetworkMsg(pmsg.Data)
 		decMsg, _ := queue.DecodeSSVMessage(msg)
