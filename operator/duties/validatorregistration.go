6d402aad93cacac1f769b6cb9986363b9bb703cb
moshe-blox <moshe@blox.io>
2024-01-16 21:01:08 +0200

    temporarily do regs every epoch, & deploy to prod prater

--- operator/duties/validatorregistration.go
+++ operator/duties/validatorregistration.go
@@ -8,7 +8,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const validatorRegistrationEpochInterval = uint64(10)
+const validatorRegistrationEpochInterval = uint64(1)
 
 type ValidatorRegistrationHandler struct {
 	baseHandler
